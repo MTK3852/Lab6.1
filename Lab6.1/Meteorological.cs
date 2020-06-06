2be41699022dcd48d8da41e0de3ccd9626cce328
MTK3852 <mtk835123@gmail.com>
2020-06-06 13:24:24 +0300

    1Task

---
+++ Lab6.1/Meteorological.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB6._1
+{
+    interface Met
+    {
+        public void printResult();
+    }
+    public class Meteorological : Met
+    {
+        private string month;
+        private double data;
+        private double data1;
+        private double airtempreture;
+        private double airtempreture1;
+
+        public Meteorological()
+        {
+            month = "None";
+            data = 0;
+            airtempreture = 0;
+        }
+
+        public Meteorological(string month, double data, double data1, double airtemperature, double airtempreture1)
+        {
+            this.month = month;
+            this.data = data;
+            this.data1 = data1;
+            this.airtempreture = airtemperature;
+            this.airtempreture1 = airtempreture1;
+
+        }
+        public Meteorological(int airtemperature, int airtempreture1)
+        {
+
+            this.airtempreture = airtemperature;
+            this.airtempreture1 = airtempreture1;
+
+        }
+        public string Month
+        {
+            get { return month; }
+            set { month = value; }
+
+        }
+        public double Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public double Data1
+        {
+            get { return data1; }
+            set { data1 = value; }
+        }
+        public double AirTemperature
+        {
+            get { return airtempreture; }
+            set { airtempreture = value; }
+        }
+        public double AirTemperature1
+        {
+            get { return airtempreture1; }
+            set { airtempreture1 = value; }
+        }
+        public void printResult()
+        {
+            double mid = (airtempreture + airtempreture1) / 2;
+            Console.WriteLine("\n| Month | Average air temperature |");
+            Console.Write("|" + month + "|" + mid + "|");
+
+        }
+
+
+
+    }
+}
